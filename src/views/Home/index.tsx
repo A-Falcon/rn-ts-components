ff1659d56bdf776fed88c63f6de567fb7174d3dd
Ahtziri <Afalcon1209@gmail.com>
2022-02-18 22:25:57 -0800

    add minimal styling to Home buttons

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -10,6 +10,7 @@ import styled from 'styled-components/native'
     const navigate = useNavigate();
     return (
       <Wrapper>
+        <Header>Select Component Demo</Header>
         <Button onPress={() => navigate("/flashcard")}>
           <ButtonText>FlashCard</ButtonText>
         </Button>
@@ -22,14 +23,24 @@ import styled from 'styled-components/native'
 }
 
 const Wrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   
+`
+const Header = styled.Text`
+  font-size: 20px;
+  color:${(props) => props.theme.colors.darkText};
 `
 const Button = styled.TouchableOpacity`
   background-color: transparent;
   padding: 20px;
+  
 `
 const ButtonText = styled.Text`
-  color:${(props) => props.theme.colors.darkText};
+  color:${(props) => props.theme.colors.lightText};
+  font-size: 20px;
 `
 
 export default Home
